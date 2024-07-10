e5a91d1b623dcacf8ebaca1109b09fc178a4c8a2
Suman Das <sumandas131299@gmail.com>
2024-07-10 16:23:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from './logo.svg';
+
+import { useCallback, useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [length , setLength] = useState(8)
+  const [number , setNumber] = useState(false)
+  const [char , setChar] = useState(false)
+  const [password , setPassword] = useState("")
+
+  const passref = useRef(null)
+
+  const passwordGenerator = useCallback(()=>{
+
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyv"
+    if(number) str+="0123456789"
+    if(char) str+="<>?:{}!@#$%^&*"
+
+    for (let index = 0; index < length; index++) {
+      pass+=str.charAt(Math.floor(Math.random()*str.length+1));
+      
+    }
+    setPassword(pass)
+
+  } , [length , number , char ,setPassword])
+
+  useEffect(()=>{passwordGenerator()},[length,number,char,passwordGenerator])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      
+      <div className='bg-blue-700 rounded-lg my-10 mx-10 px-4 py-4'>
+       <h1 className='text-center text-xl text-white'>Password Generator</h1> 
+       <div className='flex rounded-lg mb-4 overflow-hidden'>
+        <input 
+        type='text'
+        placeholder='password'
+        value={password}
+        className='py-3 px-3 w-full'
+        ref={passref}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        </input>
+        <button className=' px-3 py-1 outline-none bg-green-200' 
+        onClick={
+          useCallback(()=>{
+            
+            passref.current?.select()
+            window.navigator.clipboard.writeText(password)
+        },[password])}>
+          copy
+        </button>
+       </div>
+       <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input
+          type='range'
+          min={8}
+          max={20}
+          value={length}
+          className='cursor-pointer'
+          onChange={(e)=> {setLength(e.target.value)}}
+          >
+          </input>
+          <label>length : {length}</label>
+
+        </div>
+          <div className='flex items-center gap-x-1'>
+          <input
+          type='checkbox'
+          defaultChecked={number}
+          id='numberInput'
+          onChange={()=>{setNumber((prev)=> (!prev))}}
+          >
+          </input>
+          <label>Add_numbers</label>
+        
+
+          </div>
+
+          <div className='flex items-center gap-x-1'>
+          <input
+          type='checkbox'
+          defaultChecked={char}
+          id='numberInput'
+          onChange={()=>{setChar((prev)=> (!prev))}}
+          >
+          </input>
+          <label>Add_Special_Characters</label>
+        
+
+          </div>
+
+       </div>
+       
+       
+      </div>
+
+
+    </>
   );
 }
 
